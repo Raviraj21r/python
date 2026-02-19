8757488540c105566929c0555f0b71ee77021792
Raviraj21r <shankarkumar66363@gmail.com>
2026-02-19 22:02:53 +0530

    you

---
+++ list python/first.py
@@ -0,0 +1,8 @@
+# An empty list
+my_list = []
+
+# A list with elements (can be mixed types)
+fruits = ["apple", "banana", "cherry"]
+mixed_list = ["abc", 34, True, 40]
+print(fruits)           
+print(mixed_list)
