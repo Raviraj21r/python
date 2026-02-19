19ec9556eab173e213910b2f5084d1c80090d8d3
Raviraj21r <shankarkumar66363@gmail.com>
2026-02-19 22:35:44 +0530

    all file dic

---
+++ dictonary/dictonary.py
@@ -0,0 +1,16 @@
+# Creating a dictionary with data
+person = {
+    "name": "Alice",
+    "age": 30,
+    "role": "Engineer"
+}
+print(person)
+# Output: {'name': 'Alice', 'age': 30, 'role': 'Engineer'}
+
+# Creating an empty dictionary
+empty_dict = {}
+
+# Creating a dictionary using the dict() constructor with keyword arguments
+employee_dict = dict(name="Bob", age=25, role="Designer")
+print(employee_dict)
+# Output: {'name': 'Bob', 'age': 25, 'role': 'Designer'}
